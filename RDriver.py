4f24f003edbe24daa2b2bc33ff45caef5a2c8ea1
Joshua Comish <jcomish@sourceiron.com>
2021-07-05 13:32:40 -0600

    JC - Update README and introduce download capabilities

--- RDriver.py
+++ RDriver.py
@@ -1,16 +1,36 @@
 from selenium import webdriver
 import time
-
+from pathlib import Path
 
 class RDriver:
-    def __init__(self, driver_path, attempts=100, cooldown=100):
+    def __init__(self, driver_path, attempts=100, cooldown=100, download_dir=None, download_start_time=1):
         """
 
         :param driver_path: Path of your chromedriver executable.
         :param attempts: Amount of times to retry a method.
         :param cooldown: Amount of time (in milliseconds) between method retries.
         """
-        self.driver = webdriver.Chrome(driver_path)
+        # Set download settings
+        self.download_dir = str(Path.cwd())
+        if download_dir:
+            self.download_dir = download_dir
+        self.download_start_time = download_start_time
+
+        # Make sure that directory exists
+        if not Path(self.download_dir).exists():
+            Path(self.download_dir).mkdir()
+        if not Path(self.download_dir, "screenshots").exists():
+            Path(self.download_dir, "screenshots").mkdir()
+
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option('prefs', {
+            "download.default_directory": self.download_dir,
+            "download.prompt_for_download": False,
+            "download.directory_upgrade": True,
+            "plugins.always_open_pdf_externally": True
+        })
+
+        self.driver = webdriver.Chrome(driver_path, chrome_options=options)
         self.attempts = attempts
         self.cooldown = cooldown / 1000
 
@@ -31,14 +51,6 @@ class RDriver:
 
         return magic
 
-    def navigate(self, url):
-        """
-        Navigates to a URL.
-        :param url: The URL.
-        :return:
-        """
-        self.driver.get(url)
-
     def _find_element(self, locator, by):
         """
         (Private) Generalized function to find an element.
@@ -55,7 +67,7 @@ class RDriver:
             return self.driver.find_element_by_class_name(locator)
         if by == "css_selector":
             return self.driver.find_element_by_css_selector(locator)
-        if by == "link_text":
+        if by == "text":
             return self.driver.find_element_by_link_text(locator)
         if by == "partial_link_text":
             return self.driver.find_element_by_partial_link_text(locator)
@@ -63,6 +75,25 @@ class RDriver:
             return self.driver.find_element_by_name(locator)
         if by == "tag_name":
             return self.driver.find_element_by_tag_name(locator)
+
+    def _wait_for_download(self):
+        time.sleep(self.download_start_time)  # Wait for download to start
+        while True:
+            files = [str(x) for x in Path(self.download_dir).glob('**/*') if x.is_file()]
+            for file in files:
+                if ".crdownload" in file:
+                    break
+            else:
+                return True
+            time.sleep(self.cooldown)
+
+    def navigate(self, url):
+        """
+        Navigates to a URL.
+        :param url: The URL.
+        :return:
+        """
+        self.driver.get(url)
 
     @_retry
     def click(self, locator, by="xpath"):
@@ -96,3 +127,33 @@ class RDriver:
         :return: None
         """
         return self._find_element(locator, by)
+
+
+    @_retry
+    def download_file_from_link(self, locator, by):
+        """
+        Downloads and waits for a download from a link.
+        :param locator: Link of an element to download
+        :return:
+        """
+        self._find_element(locator, by).click()
+        self._wait_for_download()
+        return True
+
+    def download_file_from_url(self, url):
+        """
+        Downloads and waits for a download from a URL.
+        :param url: URL to trigger the download.
+        :return:
+        """
+        self.driver.get(url)
+        self._wait_for_download()
+        return True
+
+    def take_screenshot(self, filename):
+        """
+        Takes a screenshot of the page
+        :param filename: Name of the file to save locally
+        :return:
+        """
+        self.driver.save_screenshot(self.download_dir + "/screenshots/" + filename)
--- unit_test.py
+++ unit_test.py
@@ -1,4 +1,7 @@
 from RDriver import RDriver
+from pathlib import Path
+
+DOWNLOAD_DIR = str(Path.cwd())
 
 def test_click():
     driver = RDriver("/Users/comish/Projects/Scripts/chromedriver")
@@ -17,3 +20,20 @@ def test_get_data():
     driver.click("searchButton", "id")
     data = driver.get_data("firstHeading", "id")
     assert data.text == "Flag of the Isle of Man"
+
+def test_screenshot():
+    driver = RDriver("/Users/comish/Projects/Scripts/chromedriver", download_dir=DOWNLOAD_DIR)
+    driver.navigate("https://en.wikipedia.org/wiki/Manx_cat")
+    driver.take_screenshot("ss.png")
+    assert Path(DOWNLOAD_DIR, "screenshots", "ss.png").exists()
+
+def test_download_link():
+    driver = RDriver("/Users/comish/Projects/Scripts/chromedriver", download_dir=DOWNLOAD_DIR)
+    driver.navigate("https://fastest.fish/test-files")
+    driver.download_file_from_link("10MB", "text")
+    assert Path(DOWNLOAD_DIR, "10MB.zip").exists()
+
+def test_download_url():
+    driver = RDriver("/Users/comish/Projects/Scripts/chromedriver", download_dir=DOWNLOAD_DIR)
+    driver.download_file_from_url("https://s1.q4cdn.com/806093406/files/doc_downloads/test.pdf")
+    assert Path(DOWNLOAD_DIR, "test.pdf").exists()
